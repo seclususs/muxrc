c54cac47bafb97be0f78c4b3758b122718d03bb7
seclususs <antoa7767@gmail.com>
2026-09-23 13:34:12 +0700

    Fix ARP sweeper output formatting
    Format raw nmap results into a clean tabular list using awk
    as defined in the original toolkit design specification.

--- tools/sh/arp-sweeper.sh
+++ tools/sh/arp-sweeper.sh
@@ -14,7 +14,39 @@ fi
 
 echo "[*] Target Subnet: $cidr"
 echo "[*] Initiating Nmap ARP sweep..."
+echo ""
+printf "%-18s | %-19s | %s\n" "IP Address" "MAC Address" "Vendor / Hostname"
+echo "----------------------------------------------------------------------"
 
-nmap -sn "$cidr"
+nmap -sn "$cidr" | awk '
+/^Nmap scan report for/ {
+    ip = $5
+    if (ip ~ /\(/) {
+        ip = substr(ip, 2, length(ip)-2)
+        host = $5
+    } else {
+        host = ""
+        if (NF == 6) {
+            host = $5
+            ip = substr($6, 2, length($6)-2)
+        }
+    }
+}
+/^MAC Address:/ {
+    mac = $3
+    vendor = ""
+    for(i=4; i<=NF; i++) {
+        vendor = vendor $i " "
+    }
 
+    gsub(/^\(|\) $/, "", vendor)
+
+    if (host != "") {
+        printf "%-18s | %-19s | %s (%s)\n", ip, mac, vendor, host
+    } else {
+        printf "%-18s | %-19s | %s\n", ip, mac, vendor
+    }
+}'
+
+echo "----------------------------------------------------------------------"
 echo "[+] Sweep complete."
